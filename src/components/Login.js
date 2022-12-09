167218bf187ad68fe5489167b174c91848dff812
4-Leafs-Code <105990331+4-Leafs-Code@users.noreply.github.com>
2022-12-09 11:47:04 +0000

    working on guest account

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,13 +16,13 @@ const Login = (props) => {
                 </a>
                 <div>
                     <Join>Join Now</Join>
-                    <SignIn>Sign In</SignIn>
+                    <SignIn onClick = {<Redirect to = '/home' />} >Guest</SignIn>
                 </div>
             </Nav>
 
             <Section>
                 <Hero>
-                    <h1>Welcome to your professional community</h1>
+                    <h1>Welcome to the Maine Community Resources Network</h1>
                     <img src="/images/login-hero.svg" alt="" />
                 </Hero>
 
@@ -60,7 +60,7 @@ const Nav = styled.nav`
     }
 `;
 
-const Join = styled.a`
+const Join = styled.button`
     font-size: 16px;
     padding: 10px 12px;
     text-decoration: none;
@@ -77,24 +77,25 @@ const Join = styled.a`
     }
 `;
 
-const SignIn = styled.a`
-    box-shadow: inset 0 0 0 1px #0a66c2;
-    color: #0a66c2;
-    border-radius: 24px;
-    transition-duration: 167ms;
-    font-size: 16px;
-    font-weight: 600;
-    line-height: 40px;
-    padding: 12px 23px;
-    text-align: center;
-    background-color: rgba(0, 0, 0, 0);
+const SignIn = styled.button`
+        display: flex;
+        justify-content: center;
+        background-color: #fff;
+        align-items: center;
+        height: 56px;
+        width: 100%;
+        border-radius: 28px;
+        box-shadow: inset 0 0 0 1px rgba(0 0 0 / 60%), inset 0 0 0 2px rgb(0 0 0 / 0%) inset 0 0 0 1px rgb(0 0 0 / 0%);
+        vertical-align: middle;
+        z-index: 0;
+        transition-duration: 167ms;
+        font-size: 20px;
+        color: rgba(0, 0 , 0, 0.6);
 
-    &:hover {
-        background-color: rgba(112, 181, 249, 0.15);
-        color: #0a66c2;
-        text-decoration: none;
-        cursor: pointer;
-        box-shadow: inset 0 0 0 2px #0a66c2;
+        &:hover {
+            background-color: rgba(207, 207, 207, 0.25);
+            color: rgba(0, 0, 0, 0.75);
+            cursor: pointer;
     }
 `;
 
