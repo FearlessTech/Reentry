8d58df9c97f49b9970e0f8c75d6185729cc951ac
4-Leafs-Code <105990331+4-Leafs-Code@users.noreply.github.com>
2022-12-14 19:21:39 +0000

    continue working on it

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -20,11 +20,7 @@ export const Navbar = () => {
       <div className="container">
         <div className="content">
           <Link to="/">
-            <img
-              id=""
-              src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fhome-logo.svg?alt=media&token=4c8fd56b-27c2-4309-9db8-20419b262ae7"
-              alt=""
-            />
+            <img className="logoimg" src="/images/MaineRRLogo.png" alt="" />
           </Link>
 
           <div className="search">
@@ -33,7 +29,7 @@ export const Navbar = () => {
             </div>
             <div id="searchicon">
               <img
-                src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fsearch-icon.svg?alt=media&token=e35c871e-f8fc-4266-9a2b-758b26683eb8"
+                src="/images/search-icon.svg"
                 alt=""
               />
             </div>
@@ -43,7 +39,7 @@ export const Navbar = () => {
             <div>
               <Link to="/">
                 <a id="one" onClick={changeStyle}>
-                  <img src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fnav-home.svg?alt=media&token=26214d47-fb1a-47d9-974f-4b72e3a3554c" />
+                  <img src="../images/nav-home.svg" />
                   <span>Home</span>
                 </a>
               </Link>
@@ -52,7 +48,7 @@ export const Navbar = () => {
             <div>
               <Link to="/network">
                 <a id="two">
-                  <img src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fnav-network.svg?alt=media&token=2b0e6854-0573-4391-b161-58c040c466af" />
+                  <img src="/images/nav-network.svg" />
                   <span>My Network</span>
                 </a>
               </Link>
@@ -61,21 +57,21 @@ export const Navbar = () => {
             <div>
               <Link to="/jobs">
                 <a id="three">
-                  <img src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fnav-jobs.svg?alt=media&token=42c52930-c50b-4ddf-956b-ba7116712eec" />
+                  <img src="/images/nav-jobs.svg" />
                   <span>Jobs</span>
                 </a>
               </Link>
             </div>
             <div>
               <a id="four">
-                <img src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fnav-messaging.svg?alt=media&token=5c853846-b68d-4c7e-abfe-3f7162a39709" />
+                <img src="/images/nav-messaging.svg" />
                 <span>Messaging</span>
               </a>
             </div>
             <div>
               <Link to="/notifications">
                 <a id="five">
-                  <img src="https://firebasestorage.googleapis.com/v0/b/linkedin-clone-3e412.appspot.com/o/navbar%2Fnav-notifications.svg?alt=media&token=e95f4913-c388-44c9-821a-988b39e2cdd3" />
+                  <img src="/images/nav-notifications.svg" />
                   <span>Notifications</span>
                 </a>
               </Link>
--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import "firebase/compat/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC1pxEo1d7x1fsBozLFsdu5jj9L3IjWoEU",
-  authDomain: "mainereentrynetwork-d719e.firebaseapp.com",
-  projectId: "mainereentrynetwork-d719e",
-  storageBucket: "mainereentrynetwork-d719e.appspot.com",
-  messagingSenderId: "439700418780",
-  appId: "1:439700418780:web:175e3c4944713a75e14419",
-  measurementId: "G-7TRRF90XN9"
+  apiKey: "AIzaSyCKlbiyPeccZc_E5wnfYs_wApRw3gjKENY",
+  authDomain: "mainereentry.firebaseapp.com",
+  projectId: "mainereentry",
+  storageBucket: "mainereentry.appspot.com",
+  messagingSenderId: "844001971791",
+  appId: "1:844001971791:web:85349a067d13a6b32e643a",
+  measurementId: "G-P1RQH1BG36"
 };
 
 
