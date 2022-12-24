61336ff76b76e9c24eb013958a13eeb3b564942f
clover.baxter <cloversbaxter@wccc.me.edu>
2022-12-24 11:07:56 -0500

    Completed Post CSS issue

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,46 +8,46 @@ import { FaEllipsisH, FaThumbsUp } from "react-icons/fa";
 import { IoMdSettings, } from "react-icons/io";
 
 const Main = (props) => {
-    const [showModal, setShowModal] = useState("close");
-    
-    useEffect(() => {
-        props.getArticles()
-    }, []);
+  const [showModal, setShowModal] = useState("close");
 
-    const handleClick = (e) => {
-        e.preventDefault();
-        if(e.target !== e.currentTarget) {
-            return;
-        }
+  useEffect(() => {
+    props.getArticles()
+  }, []);
 
-        switch(showModal) {
-            case "open":
-                setShowModal("close");
-                break;
-            case "close":
-                setShowModal("open");
-                break;
-            default:
-                setShowModal("close");
-                break;
-        }
+  const handleClick = (e) => {
+    e.preventDefault();
+    if(e.target !== e.currentTarget) {
+      return;
     }
 
-    return (
-      <Container>
-        <PostNav>
-          <button 
-            onClick = {handleClick}
-            disabled = { !!props.loading }
-            className="post-space">
-            Start a Post    
-          </button> 
-          <IoMdSettings size={30} style={{margin: '8px', fill:'#cdcdcd'}}></IoMdSettings>
-        </PostNav>
-        <Content>
-          {props.loading && <img src="public/images/spin-loading.gif" alt=""/>}
-          {props.articles.length != 0 &&
-            props.articles.map((article, key) => (
+    switch(showModal) {
+      case "open":
+        setShowModal("close");
+        break;
+      case "close":
+        setShowModal("open");
+        break;
+      default:
+        setShowModal("close");
+        break;
+    }
+  }
+
+  return (
+    <Container>
+      <PostNav>
+        <button
+          onClick = {handleClick}
+          disabled = { !!props.loading }
+          className="post-space">
+          Start a Post
+        </button>
+        <IoMdSettings size={30} style={{margin: '8px', fill:'#99d3df'}}></IoMdSettings>
+      </PostNav>
+      <Content>
+        {props.loading && <img src="public/images/spin-loading.gif" alt=""/>}
+        {props.articles.length != 0 &&
+          props.articles.map((article, key) => (
             <Article key = {key} >
               <SharedActor>
                 <a>
@@ -58,12 +58,12 @@ const Main = (props) => {
                     <span>{article.actor.date.toDate().toLocaleDateString()}</span>
                   </div>
                 </a>
-                <button><FaEllipsisH /></button>
+                <button><FaEllipsisH size={20} style={{margin: '8px', fill:'#99d3df'}} /></button>
               </SharedActor>
               <Description>{article.description}</Description>
               <SharedImage>
                 <a>
-                  {!article.sharedImg && article.video ? 
+                  {!article.sharedImg && article.video ?
                     (<ReactPlayer width = {'100%'} url={article.video} />)
                     :
                     (article.sharedImg && <img src={article.sharedImg} alt=""/>)
@@ -71,8 +71,8 @@ const Main = (props) => {
                 </a>
               </SharedImage>
               <SocialCounts>
-                <li><button><FaThumbsUp/></button></li>
-                <li><a>{article.comments} comments</a></li>
+                <li><a>{article.likes} Likes</a></li>
+                <li><a>{article.comments} Comments</a></li>
               </SocialCounts>
               <SocialActions>
                 <button><span>Like</span></button>
@@ -80,35 +80,35 @@ const Main = (props) => {
                 <button><span>Share</span></button>
                 <button><span>Send</span></button>
               </SocialActions>
-            </Article>        
-            ))        
-          }
-        </Content>
-        <PostModal showModal={showModal} handleClick = {handleClick} />
-      </Container>
-    );
+            </Article>
+          ))
+        }
+      </Content>
+      <PostModal showModal={showModal} handleClick = {handleClick} />
+    </Container>
+  );
 };
 
 const Container = styled.div`
-    grid-area: main;
+  grid-area: main;
 `;
 
 const CommonCard = styled.div`
-    text-align: center;
-    overflow: hidden;
-    margin-bottom: 8px;
-    background-color: #e9e9e9;
-    border-radius: 5px;
-    position: relative;
-    border: none;
-    border-radius: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #e9e9e9;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  border-radius: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
 `;
 
 const Article = styled(CommonCard)`
-    padding: 0;
-    margin: 0 0 8px;
-    overflow: visible;
-    
+  padding: 0;
+  margin: 0 0 8px;
+  overflow: visible;
+
 `;
 
 const SharedActor = styled.div`
@@ -118,19 +118,18 @@ const SharedActor = styled.div`
   align-items: center;
   display: flex;
   background-color: #e9e9e9;
-
+  border-radius: 5px;
   a {
     margin-right: 12px;
     flex-grow: 1;
     overflow: hidden;
     display: flex;
     text-decoration: none;
-
     img {
       width: 48px;
       height: 48px;
+      border-radius: 5px;
     }
-
     & > div {
       display: flex;
       flex-direction: column;
@@ -138,16 +137,13 @@ const SharedActor = styled.div`
       flex-basis: 0;
       margin-left: 8px;
       overflow: hidden;
-
       span {
         text-align: left;
-
         &:first-child {
           font-size: 14px;
           font-weight: 700;
           color: rgba(0, 0, 0, 1);
         }
-
         &:nth-child(n+1) {
           font-size: 12px;
           color: rgba(0, 0, 0, 0.6);
@@ -155,7 +151,6 @@ const SharedActor = styled.div`
       }
     }
   }
-
   button {
     position: absolute;
     right: 12px;
@@ -167,85 +162,85 @@ const SharedActor = styled.div`
 `;
 
 const Description = styled.div`
-    padding: 0 16px;
-    overflow: hidden;
-    color: rgba(0,0,0,0.9);
-    font-size: 14px;
-    text-align: left;
+  padding: 0 16px;
+  overflow: hidden;
+  color: rgba(0,0,0,0.9);
+  font-size: 14px;
+  text-align: left;
 `;
 
 const SharedImage = styled.div`
-    margin-top: 8px;
-    width: 100%;
-    display: block;
-    position: relative;
-    background-color: #f9fafb;
+  margin-top: 8px;
+  width: 100%;
+  display: block;
+  position: relative;
+  background-color: #e9e9e9;
+  
 
-    img {
-        object-fit: contain;
-        width: 100%;
-        height: 100%;
-    }
+  img {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+    border-radius: 5px;
+  }
 `;
 
 const SocialCounts = styled.ul`
-    line-height: 100%;
-    display: flex;
-    align-items: flex-start;
-    overflow: auto;
-    list-style: none;
-    margin: 0 16px;
-    padding: 8px 0;
-    border-bottom: 1px solid #e9e5df;
+  line-height: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: flex-start;
+  overflow: auto;
+  list-style: none;
+  margin: 0 16px;
+  padding: 8px 0;
+  border-bottom: 1px solid #e9e5df;
 
-    li {
-        margin-right: 5px;
-        font-size: 12px;
-
-        button {
-            display: flex;
-            border: none;
-            background: #fff;
-        }
-    }
-
-    img {
-        width: 18px;
-    }
+  li {
+    margin-right: 5px;
+    font-size: 12px;
+  }
 `;
 
 const SocialActions = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  min-height: 40px;
+  padding: 4px 8px;
+  border-radius: 5px;
+
+  button {
+    display: inline-flex;
     align-items: center;
-    display: flex;
-    justify-content: center;
-    margin: 0;
-    min-height: 40px;
-    padding: 4px 8px;
+    padding: 8px;
+    color: #88BBD6;
+    border: none;
+    background-color: #FCF4A3;
+    border-radius: 5px;
+    margin: 5px;
+    text-align: center;
 
-    button {
-        display: inline-flex;
-        align-items: center;
-        padding: 8px;
-        color: #0a66c2;
-        border: none;
-        background-color: #fff;
-
-        @media (min-width: 768px) {
-            span {
-                margin-left: 8px;
-            }
-        }
+    @media (min-width: 768px) {
+      span {
+        margin: 3px;
+      }
     }
+    &:hover {
+      background-color: #FFFDD0;
+    }
+  }
 `;
 
 const Content = styled.div`
-    text-align: center;
-    & > img {
-        width: 30px;
-    }
+  text-align: center;
+  & > img {
+    width: 30px;
+  }
 `;
 const PostNav = styled.div`
-  background-color: #FCF4a3;
+  background-color: #FCF4A3;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -271,21 +266,21 @@ const PostNav = styled.div`
       }
     }
     &:hover {
-        background-color: #88bbd6;
+      background-color: #88bbd6;
     }
   }
 `;
 
 const mapStateToProps = (state) => {
-    return {
-        loading: state.articleState.loading,
-        user: state.userState.user,
-        articles: state.articleState.articles,
-    }
+  return {
+    loading: state.articleState.loading,
+    user: state.userState.user,
+    articles: state.articleState.articles,
+  }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    getArticles: () => dispatch(getArticlesAPI()),
+  getArticles: () => dispatch(getArticlesAPI()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps) (Main);
