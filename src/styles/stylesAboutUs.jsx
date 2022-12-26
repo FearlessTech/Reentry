80609dea20a3e896039bd2c70800a7b1ff56daee
clover.baxter <cloversbaxter@wccc.me.edu>
2022-12-25 22:15:36 -0500

    made pages and setup styles sheets

---
+++ src/styles/stylesAboutUs.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding-top: 52px;
+  max-width: 100%;
+`;
+
+const Content = styled.div`
+  max-width: 1120px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+  display: flex;
+`;
+
+const Image = styled.div`
+  width: 25%;
+`;
+
+const Info = styled.div`
+  width: 75%;
+`;
+
+export {
+  Container,
+  Content,
+  Image,
+  Info,
+};
