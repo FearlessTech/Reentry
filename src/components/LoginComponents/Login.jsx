d2a7472f9fb9bd9b42cec5b3963983a17cd97343
= <rondinelibrt18@gmail.com>
2022-12-30 03:41:07 -0300

    style: login page demo

---
+++ src/components/LoginComponents/Login.jsx
@@ -0,0 +1,9 @@
+import StyledLogin from './LoginStyles/styledLogin';
+
+export default function Login(props) {
+  return (
+    <StyledLogin>
+      <div>hello sign</div>
+    </StyledLogin>
+  );
+}
---
+++ src/components/LoginComponents/LoginStyles/styledSignup.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+const StyledSignUp = styled.div`
+
+`;
+
+export default StyledSignUp;
