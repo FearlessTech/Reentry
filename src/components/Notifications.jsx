1ca361707025be4455eb75231d717faa2bcb7661
= <rondinelibrt18@gmail.com>
2023-01-03 20:24:58 -0300

    style: notification clean up

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -3,8 +3,6 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import {
   Container,
   Left,
-  LeftDiv,
-  HR,
   Middle,
   MiddleBoxes,
   MiddleContent,
@@ -60,83 +58,77 @@ const Notifications = () => {
 
   return (
     <Container>
-      <Left>
-        <LeftDiv>
-          <h1>Notifications</h1>
-          <p>You have new notifications</p>
-        </LeftDiv>
-        <LeftDiv>
-          <p>Improve your notifications</p>
-          <h1>View settings</h1>
-        </LeftDiv>
-      </Left>
+      <div className='container'>
+        <Left>
+          <div>
+            <h1>Notifications</h1>
+            <p>You have new notifications</p>
+          </div>
+          <hr />
+          <div>
+            <h1>View settings</h1>
+            <p>Improve your notifications</p>
+          </div>
+        </Left>
 
-      <Middle>
-        {notifications.map((notification) => {
-          return (
-            <MiddleBoxes key={notification.id}>
-              <div className='img-wrapper'>
-                <img src={notification.image?.url} alt='' />
-              </div>
-              <MiddleContent>
-                <div className='prevMessage'>
-                  <Link to={`/notifications/${notification.id}`}>
-                    {notification.prevMessage}
-                  </Link>
+        <Middle>
+          {notifications.map((notification) => {
+            return (
+              <MiddleBoxes key={notification.id}>
+                <div className='img-wrapper'>
+                  <img src={notification.image?.url} alt='' />
                 </div>
-                <div className='action'>
-                  <Link to={`/notifications/${notification.id}`}>
-                    {categoryText(notification.category)}
-                  </Link>
-                </div>
-              </MiddleContent>
-              <Dots >
-                <NotificationsDropdown
-                  notifications={notifications}
-                  setNotifications={setNotifications}
-                  id={notification.id}
-                />
-                <span className='time'>1d</span>
-              </Dots>
-            </MiddleBoxes>
-          );
-        })}
-      </Middle>
+                <MiddleContent>
+                  <div className='prevMessage'>
+                    <Link to={`/notifications/${notification.id}`}>
+                      {notification.prevMessage}
+                    </Link>
+                  </div>
+                  <div className='action'>
+                    <Link to={`/notifications/${notification.id}`}>
+                      {categoryText(notification.category)}
+                    </Link>
+                  </div>
+                </MiddleContent>
+                <Dots>
+                  <NotificationsDropdown
+                    notifications={notifications}
+                    setNotifications={setNotifications}
+                    id={notification.id}
+                  />
+                  <span className='time'>1d</span>
+                </Dots>
+              </MiddleBoxes>
+            );
+          })}
+        </Middle>
 
-      <Right>
-        <RTopDiv>
-          <Ads>
-            {' '}
-            <p>Ad</p> <MoreHorizIcon />
-          </Ads>
-          <p>100 years of Japanese innovation built to meet all your needs</p>
-          <RightImg>
-            <img
-              className='circle'
-              src='https://media-exp1.licdn.com/dms/image/C4E03AQHYRKYBeIM7Rg/profile-displayphoto-shrink_100_100/0/1642222680344?e=1651104000&v=beta&t=lJofr2NhWemgVoiK1h6bFuVVvteMebPllDY5FFsmHWk'
-              alt=''
-            />
-            <img
-              src='https://media-exp1.licdn.com/dms/image/C510BAQEUO8H0EBcAVQ/company-logo_100_100/0/1543490756062?e=1654128000&v=beta&t=ZrRrKqq3xAnO6z_2i_eDgcaxj9z19cBvszX6BqUzkMY'
-              alt=''
-            />
-          </RightImg>
-          <p>A powerhouse for all your printing needs!</p>
-          <button> Follow</button>
-        </RTopDiv>
-        <RBottomBox>
-          <span>
-            About | Accessibility | Help Center | Privarcy & Terms ⮟ | Ad
-            Choices | Advertising | Business Services ⮟ | Get the MaineRR app |
-            More
-          </span>
+        <Right>
+          <RTopDiv>
+            <Ads>
+              <p>Ad</p> <MoreHorizIcon />
+            </Ads>
+            <p>100 years of Japanese innovation built to meet all your needs</p>
+            {/* <RightImg>
+            </RightImg> */}
+            <hr />
+            <p>A powerhouse for all your printing needs!</p>
+            <button> Follow</button>
+          </RTopDiv>
+          <RBottomBox>
+            <span>
+              About | Accessibility | Help Center | Privarcy & Terms ⮟ | Ad
+              Choices | Advertising | Business Services ⮟ | Get the MaineRR app
+              | More
+            </span>
 
-          <RBLink>
-            <img src='/images/RRLogo.png' alt='' />
-            <p> Maine Reentry Resources © 2022</p>
-          </RBLink>
-        </RBottomBox>
-      </Right>
+            <RBLink>
+              <img src='/images/RRLogo.png' alt='' />
+              <p> Maine Reentry Resources © 2022</p>
+            </RBLink>
+          </RBottomBox>
+        </Right>
+      </div>
     </Container>
   );
 };
