400701aa247a48c3408d62373be7809719df484d
clover.baxter <cloversbaxter@wccc.me.edu>
2023-01-03 09:48:36 -0500

    Placing ing the new login page in place

--- src/styles/stylesLogin.jsx
+++ src/styles/stylesLogin.jsx
@@ -1,29 +1,7 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  //padding: 2rem;
   height: 100vh;
-  background-size: cover;
-  background: radial-gradient(
-          ellipse 50% 90%,
-          #FFFBE0 5%,
-          #FFF3A7 20%,
-          #DFBAAA 50%,
-          #FAE2C0 70%,
-          #BFDDBF
-  );
-  //display: block;
-  //& ::selection {
-  //  color: white;
-  //  background: #c4640a;
-  //}
-  //@media (prefers-color-scheme: dark) {
-  //  background-color: #3c585839;
-  //  & ::selection {
-  //    color: #3b3b3b;
-  //    background-color: #009999;
-  //  }
-}
 `;
 
 const Nav = styled.nav`
@@ -35,14 +13,12 @@ const Nav = styled.nav`
   padding: 0 3rem;
   background-color: #2a3e3e39;
   transition: ease-in-out 120ms;
-  border-radius: 2rem;
   @media (prefers-color-scheme: dark) {
     box-shadow: 0px 0px 2px #afafaf6b;
     background-color: #3c585839;
   }
   &:hover {
     background-color: #00999947;
-    border-radius: 3rem;
     @media (prefers-color-scheme: dark) {
       box-shadow: 0px 0px 1px #4848486b;
       background-color: #3c585839;
@@ -87,56 +63,9 @@ const Nav = styled.nav`
   }
 `;
 const Join = styled.button``;
+
 const LoginBtn = styled.button``;
 
-const Hero = styled.div`
-  position: relative;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  //top: 0;
-  //left: -10px;
-  //z-index: -1;
-  //min-height: 100vh;
-  //@media (prefers-color-scheme: dark) {
-  //  background-color: #222222;
-  //}
-
-  //@keyframes freedom {
-  //  from {
-  //    top: -7px;
-  //  }
-  //  to {
-  //    top: 7px;
-  //  }
-  //}
-
-  //margin-top: 8rem;
-  //.text {
-  //  font-size: 3.5rem;
-  //  padding: 3rem;
-  //  color: #009999;
-  //}
-
-  h1 {
-    z-index: 1;
-    position: absolute;
-    color: #fff;
-    font-size: 50px;
-    text-transform: uppercase;
-    font-weight: 700;
-    letter-spacing: 8px;
-    line-height: 60px;
-    span {
-      font-size: 32px;
-      font-weight: 300;
-      letter-spacing: 10px;
-    }
-  }
-`;
 const HeroImg = styled.img`
   position: absolute;
   width: 40%;
@@ -147,19 +76,13 @@ const HeroImg = styled.img`
 `;
 const HeroBird = styled.img`
   position: absolute;
-  width: 15%;
-  height: 15%;
   top: 0;
   left: 0;
-  object-fit: contain;
 `;
 
 const Section = styled.section`
-  //display: flex;
-  //justify-content: end;
   height: 90%;
   width: 100%;
-  //padding-top: 2rem;
 `;
 
 const Form = styled.div`
@@ -205,4 +128,131 @@ const Close = styled.button`
   margin-left: 80%;
   margin-top: 1rem;
 `;
-export { Container, Nav, Join, LoginBtn, Section, Hero, HeroImg, HeroBird, Form, Google, Close };
+
+const Hero = styled.div`
+  position: relative;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    z-index: 1;
+    position: absolute;
+    color: #fff;
+    font-size: 50px;
+    text-transform: uppercase;
+    font-weight: 700;
+    letter-spacing: 8px;
+    line-height: 60px;
+    span {
+      font-size: 32px;
+      font-weight: 300;
+      letter-spacing: 10px;
+    }
+  }
+  .assets-container {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -5;
+    background: radial-gradient(
+            ellipse 50% 90%,
+            #fffbe0 5%,
+            #fff3a7 20%,
+            #dfbaaa 50%,
+            #fae2c0 70%,
+            #bfddbf
+    );
+    .chain {
+      transition: 400ms cubic-bezier(0.83, 0.81, 0.36, 0.37);
+      display: flex;
+      align-items: center;
+      width: 300px;
+      height: min-content;
+      position: absolute;
+      padding-top: 50px;
+      top: 270px;
+      left: 0;
+
+      img {
+        width: 130%;
+      }
+    }
+    .birds {
+      width: 400px;
+      height: 100%;
+      .bird {
+        transition: 400ms cubic-bezier(0.83, 0.81, 0.36, 0.37);
+        position: absolute;
+        img {
+          width: 100%;
+        }
+        &.bird-12 {
+          top: 265px;
+          left: 377px;
+        }
+        &.bird-11 {
+          top: 318px;
+          left: 377px;
+        }
+        &.bird-10 {
+          top: 275px;
+          left: 440px;
+        }
+        &.bird-09 {
+          top: 287px;
+          left: 508px;
+        }
+        &.bird-08 {
+          top: 294px;
+          left: 595px;
+        }
+        &.bird-07 {
+          top: 215px;
+          left: 542px;
+        }
+        &.bird-06 {
+          top: 215px;
+          left: 724px;
+        }
+        &.bird-05 {
+          top: 200px;
+          left: 806px;
+        }
+        &.bird-04 {
+          top: 167px;
+          left: 457px;
+        }
+        &.bird-03 {
+          top: 41px;
+          left: 744px;
+        }
+        &.bird-02 {
+          top: 65px;
+          left: 925px;
+        }
+        &.bird-01 {
+          top: 10px;
+          left: 619px;
+        }
+      }
+    }
+  }
+`;
+export {
+  Container,
+  Nav,
+  Join,
+  LoginBtn,
+  Section,
+  Hero,
+  HeroImg,
+  HeroBird,
+  Form,
+  Google,
+  Close,
+};
