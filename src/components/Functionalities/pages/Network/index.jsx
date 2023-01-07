6ce0fceef6b294d39e48167f5bfaa784d730f3ee
= <rondinelibrt18@gmail.com>
2023-01-07 12:00:34 -0300

    functionalities route fixed

---
+++ src/components/Functionalities/pages/Network/index.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react';
+import { Container } from './styled';
+
+const Parallax = (props) => {
+  useEffect(() => {
+  });
+
+  return (
+    <Container>
+      test
+    </Container>
+  );
+};
+
+export default Parallax;
