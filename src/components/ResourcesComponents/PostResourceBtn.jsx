96a3c107290e99e4ed21735092485b8e2253b2ae
martinyis <martinyis11@gmail.com>
2023-01-10 12:36:38 -0500

    Add Resources Page (just design)

---
+++ src/components/ResourcesComponents/PostResourceBtn.jsx
@@ -0,0 +1,7 @@
+import styles from "../Resources.module.css";
+
+const PostResourceBtn = () => {
+  return <button className={styles.postResource}>Post a Resource</button>;
+};
+
+export default PostResourceBtn;
