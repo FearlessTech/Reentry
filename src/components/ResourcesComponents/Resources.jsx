e9eee8173b6b73dd79958b39d8e43732e89fd4f8
= <rondinelibrt18@gmail.com>
2023-01-18 17:49:18 -0300

    fix: image strech and overflow in resources page

--- src/components/ResourcesComponents/Resources.jsx
+++ src/components/ResourcesComponents/Resources.jsx
@@ -1,12 +1,131 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Resources.module.css";
 import ResourceOutput from "./ResourceOutput";
-import ResourcesItem from "./ResourcesItem";
+import ResourceItem from "./ResourceItem";
 import { SearchResults } from "../Search/SearchResults";
-import PostModal from "../Posts/PostModal";
 import { ResourcePopup } from "./ResourcePopup";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 3rem;
+  column-gap: 3rem;
+  padding: 2rem;
+  &,
+  * {
+    box-sizing: border-box;
+    font-family: Inter, sans-serif;
+  }
+
+  .search-box {
+    padding: 1rem;
+    padding-top: 3rem;
+    background-color: #d9d9d9;
+
+    .search-container {
+      .resource-search {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        .search-bar {
+          position: relative;
+          width: 100%;
+          .input-container {
+            width: 100%;
+            input {
+              width: 100%;
+            }
+          }
+          .icon-wrapper {
+          }
+        }
+        .search-results {
+          background-color: #fff;
+          width: 100%;
+          padding: 1rem;
+          height: max-content;
+          position: absolute;
+          top: 35px;
+        }
+      }
+    }
+  }
+  .items {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    .items-wrapper {
+      margin: auto;
+      background-color: #d9d9d9;
+      display: flex;
+      flex-wrap: wrap;
+      padding: 1rem;
+      column-gap: 2rem;
+      row-gap: 1rem;
+    }
+  }
+`;
+
 const Resources = (props) => {
   const [showModal, setShowModal] = useState("close");
+
+  const resources = [
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+    {
+      resourceImage: "https://via.placeholder.com/200",
+      resourceType: "Resource Type Available",
+      description: "Amount of resource description that wll fit in this div",
+    },
+  ];
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -28,11 +147,12 @@ const Resources = (props) => {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.searchBox}>
-        <div className={styles.searchDiv}>
+    <Container>
+      <div className="search-box">
+        <div className="search-container">
           <SearchResults />
         </div>
+
         <button
           className={styles.postResource}
           onClick={handleClick}
@@ -47,23 +167,23 @@ const Resources = (props) => {
           <ResourceOutput />
         </div>
       </div>
-      <div className={styles.items}>
-        <div className={styles.itemsBox}>
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
-          <ResourcesItem />
+      <div className="items">
+        <div className="items-wrapper">
+          {resources.map(({ resourceType, resourceImage, description }) => {
+            return (
+              <ResourceItem
+                type={resourceType}
+                imageSrc={resourceImage}
+                description={description}
+              />
+            );
+          })}
         </div>
       </div>
       {showModal === "open" && (
         <ResourcePopup showModal={showModal} handleClick={handleClick} />
       )}
-    </div>
+    </Container>
   );
 };
 
