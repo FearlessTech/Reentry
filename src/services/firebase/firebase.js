a3c903e47635e0e04bfb54df131da9ac4b87d220
= <rondinelibrt18@gmail.com>
2023-04-25 21:45:27 -0300

    move-firebase-to-services-folder

---
+++ src/services/firebase/firebase.js
@@ -0,0 +1,33 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCKlbiyPeccZc_E5wnfYs_wApRw3gjKENY",
+  authDomain: "mainereentry.firebaseapp.com",
+  projectId: "mainereentry",
+  storageBucket: "mainereentry.appspot.com",
+  messagingSenderId: "844001971791",
+  appId: "1:844001971791:web:85349a067d13a6b32e643a",
+  measurementId: "G-P1RQH1BG36",
+};
+
+// here i am using .env.local file to access firebase configuration;
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+// No need to change anything below this line
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebaseApp.storage();
+
+export { auth, provider, storage, db };
+export default db;
